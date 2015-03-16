8091430c040ed0b305a3878bd80867042a12dc82
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2015-03-16 21:33:19 +0800

    Fixed an erronous additional attribute that was added to waypoint.

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20150316121309) do
 
   create_table "waypoints", force: :cascade do |t|
     t.decimal  "lat",        precision: 15, scale: 13
-    t.decimal  "decimal",    precision: 15, scale: 13
     t.decimal  "long",       precision: 15, scale: 13
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
